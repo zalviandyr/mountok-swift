78efa40ab01e141198f6533a53fbd89419a453e8
Zukron Alviandy <zukronalviandy@gmail.com>
2025-05-06 10:18:26 +0700

    Initial Commit

---
+++ Mountok/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mountok
+//
+//  Created by Zukron Alviandy on 06/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mountok/MountokApp.swift
@@ -0,0 +1,17 @@
+//
+//  MountokApp.swift
+//  Mountok
+//
+//  Created by Zukron Alviandy on 06/05/25.
+//
+
+import SwiftUI
+
+@main
+struct MountokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
